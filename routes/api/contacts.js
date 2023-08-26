05ebc059c828aa7c0d3564ce0f7de665b4429527
Oleksandra Andrushchenko <oleksandraandrusenko6@gmail.com>
2023-08-26 21:56:17 +0300

    refactor on Mongoose methods

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,32 @@ const express = require('express');
 
 const contactsController = require('../../controllers/contacts-controller');
 
-const schemas = require('../../utils/validation/contacts-schemas');
-
+const {schemas} = require('../../models/contact');
 const { validateBody } = require('../../decorators');
+const { isValidId } = require('../../middlewares');
 
 const router = express.Router();
 
 router.get('/', contactsController.getAllContacts);
 
-router.get('/:contactId', contactsController.getContactById);
+router.get('/:contactId', isValidId, contactsController.getContactById);
 
 router.post('/', validateBody(schemas.contactAddSchema), contactsController.addContact);
 
-router.delete('/:contactId', contactsController.removeContact);
+router.delete('/:contactId', isValidId, contactsController.removeContact);
 
-router.put('/:contactId', validateBody(schemas.contactAddSchema), contactsController.updateContact);
+router.put(
+	'/:contactId',
+	isValidId,
+	validateBody(schemas.contactAddSchema),
+	contactsController.updateContact
+);
+
+router.patch(
+	'/:contactId/favorite',
+	isValidId,
+	validateBody(schemas.updateFavoriteSchema),
+	contactsController.updateStatusContact
+);
 
 module.exports = router;
